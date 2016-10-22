9872fba08bc738e34c384f7c67bdf27eaf87d7b0
Hiromu Yakura <hiromu1996@gmail.com>
2016-10-22 15:53:11 +0900

    update deploy.sh to specify hosts

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,15 @@
 set -ex
 
 USERNAME=$USER
+HOSTS=("$@")
+
+if [[ -z "$HOSTS" ]]; then
+    HOSTS=(isu02 isu03 isu04 isu05)
+fi
 
 curl -k -X POST --data-urlencode "payload={\"text\":\"$USERNAME started deploy\"}" https://hooks.slack.com/services/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
 
-for HOST in isu02 isu03 isu04 isu05; do
+for HOST in $HOSTS; do
   ssh isucon@$HOST -i isucon6f.key "cd /home/isucon/webapp && git pull && sudo docker-compose -f docker-compose.yml build && sudo systemctl restart isu.service && sudo sysctl -p && curl -X POST --data-urlencode \"payload={\\\"text\\\":\\\"deploy to $HOST successful (\$(git rev-parse HEAD))\\\"}\" https://hooks.slack.com/services/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"
 done
 
